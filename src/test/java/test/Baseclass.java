65d684447595749fa733e3e96c62e31f2c5112b1
VirajThale <virajthale12@gmail.com>
2021-11-11 04:00:59 +0000

    Jenkins v1

---
+++ src/test/java/test/Baseclass.java
@@ -0,0 +1,62 @@
+package test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
+public class Baseclass 
+
+{
+	public static ExtentReports report;
+	public static ExtentTest test;
+	public static WebDriver dr;
+
+	XSSFWorkbook wbook;
+	XSSFSheet sheet;
+	
+	@BeforeTest
+	public void ReportSetup() throws IOException {
+		report =new ExtentReports("ExtentReport.html");
+		FileInputStream  fis= new FileInputStream ("exceldata.xlsx");
+		wbook = new XSSFWorkbook(fis);
+		sheet= wbook.getSheet("data");
+	}
+	
+	@BeforeMethod
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver","chromedriver");
+		
+	    dr =new ChromeDriver();
+		dr.get("https://www.simplilearn.com/");
+		
+		dr.manage().window().maximize();
+		dr.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
+		
+	}
+	
+	@AfterMethod
+	public void teardown() {
+		dr.quit();
+}
+	
+	@AfterTest
+	public void ReportTearDown() throws IOException {
+		report.flush();
+		report.close();
+		wbook.close();
+		
+	}
+
+}
---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,73 @@
+package test;
+
+
+
+import java.io.IOException;
+
+
+//import static org.testng.Assert.assertEquals;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import pages.LoginPage ;
+
+public class LoginTest extends Baseclass
+
+
+{
+	
+	
+	
+
+	@Parameters({"Username","Password"})
+	@Test(enabled=true)
+	public void NegativeLogin(String UsernameVal, String PasswordVal) 
+	
+	{
+		test= report.startTest("Negative Login Test");
+		LoginPage login = new LoginPage();
+		login.Login(UsernameVal, PasswordVal);
+		report.endTest(test);
+		
+		
+	}
+	
+	@Parameters({"Username","CPassword"})
+	@Test(enabled= true)
+	public void PositiveLogin(String UsernameVal, String PasswordVal) {
+		
+		test= report.startTest("Positive Login Test");
+		LoginPage login = new LoginPage();
+		login.Login(UsernameVal, PasswordVal);
+		report.endTest(test);
+		
+	}
+	
+	@Test
+	public void ExcelTest() throws IOException {
+		
+		
+		String UsernameVal =sheet.getRow(1).getCell(0).getStringCellValue();
+		String PasswordVal =sheet.getRow(1).getCell(1).getStringCellValue();
+		
+		test= report.startTest("Negative Login Test");
+		LoginPage login = new LoginPage();
+		login.Login(UsernameVal, PasswordVal);
+		report.endTest(test);
+		
+	
+	}
+		
+	
+	
+	
+	
+
+}
+
